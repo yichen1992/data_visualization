b7f5ceac7e50067358b1dfffff4b2c8b3f9f1af9
yichen1992 <yichen1992@users.noreply.github.com>
2017-04-03 16:05:09 -0400

    .

---
+++ animate.php
@@ -0,0 +1,183 @@
+<html>
+  <head>
+    <style>
+      .button {
+          background-color: #008CBA; /* Blue */
+          border: none;
+          color: white;
+          padding: 15px 32px;
+          margin-left: 20px;
+          text-align: center;
+          text-decoration: none;
+          display: inline-block;
+          font-size: 16px;
+      }
+      .button:hover {
+          background-color: #66bad5;
+      }
+    </style>
+    <!--Load the AJAX API-->
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+
+    // Load the Visualization API and the corechart package.
+    google.charts.load('current', {'packages':['corechart']});
+
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.charts.setOnLoadCallback(drawSheetName);
+
+    // Callback that pulls data from Google Sheet,
+    // instantiates the chart, passes in the data and
+    // draws it.
+
+    function drawSheetName() {
+      var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1i0aZOeuBpe4ny1DwzSHiNBmGosSZpwWiYB1ZRn8OUUo/edit');
+      query.setQuery('SELECT * LIMIT 366');
+      query.send(handleSampleDataQueryResponse);
+    }
+
+    function handleSampleDataQueryResponse(response) {
+      if (response.isError()) {
+        alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+        return;
+      }
+
+      var initial_options = {
+        height: 600,
+        //curveType: 'function',
+        vAxis: { 
+          minValue: 0,
+          title: 'Temperature (F)',
+          maxValue: 100
+        },
+        legend: 'none',
+        series: {
+          0: { color: '#dbdbdb', lineDashStyle: [4, 4] }, 
+          1: { color: '#ff4c4c' }
+        },
+        hAxis: {
+          title: 'Month'
+        }
+      };
+      
+      var data = response.getDataTable();
+      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+      var view = new google.visualization.DataView(data);
+
+      // set columns initially
+      view.setColumns([0,1,2]);
+
+      // draw the chart initially
+      chart.draw(view, initial_options);
+      
+      // draw each new series
+      var i = 0, howManyTimes = data.getNumberOfColumns() - 2;
+      var arr = [0,1];
+
+      function updateChart() {
+          if (i < howManyTimes ){
+            arr.push(i+2);
+          }
+          view.setColumns(arr);
+
+          // odd number columns are the averages - want dashed lines, light grey #e9e9e9
+          // even number columns are the actuals - solid lines, light red #ffdbdb
+          // current line - solid red, #ff4c4c
+
+          var seriesObj = {};
+          console.log("i = " + i);
+          // loop to build series object
+          for (var j = 0; j < i; j++) {
+            
+            console.log("j = " + j);
+            console.log("mod j = " + (j % 2));
+            var innerObj = {};
+            if ((j % 2) === 0) {
+              innerObj["color"] = '#e9e9e9';
+              innerObj["lineDashStyle"] = [4,1];
+            }
+            else {
+              innerObj["color"] = '#ffdbdb';
+              innerObj["lineDashStyle"] = [0,0];
+            }
+            seriesObj[j] = innerObj;
+          }
+
+          // add last items
+          var innerObjFinal1 = {};
+          if (((i) % 2) === 0) {
+              innerObjFinal1["color"] = '#999999';
+            }
+            else {
+              innerObjFinal1["color"] = '#ff4c4c';
+            }
+          seriesObj[i] = innerObjFinal1;
+
+          var innerObjFinal2 = {};
+          if (((i+1) % 2) === 0) {
+              innerObjFinal2["color"] = '#999999';
+            }
+            else {
+              innerObjFinal2["color"] = '#ff4c4c';
+            }
+          seriesObj[i+1] = innerObjFinal2;
+
+          console.log(seriesObj);
+
+
+          var update_options = {
+            vAxis: { 
+              minValue: 0, 
+              maxValue: 100,
+              title: 'Temperature (F)'
+            },
+            legend: 'none',
+            series: seriesObj,
+            hAxis: {
+              title: 'Month'
+            }
+          };
+
+          drawChart(view,update_options);
+          i++;
+          if( i < howManyTimes ){
+              setTimeout( updateChart, 80 );
+              setTimeout( updateYear(i), 80 );
+          }
+      }
+      function updateYear(i) {
+        var year = ((i%2) === 0) ? (i/2) : (i/2 - 0.5);
+        //console.log(i);
+        document.getElementById('chart_year').innerHTML = 1995 + year;
+      }
+      updateChart();
+      
+      function drawChart(view,options) {
+        chart.draw(view, options);
+      }
+      
+      var button = document.getElementById('b1');
+
+      button.onclick = function() {
+        drawSheetName();
+      }
+    }
+    </script>
+    
+  </head>
+
+  <body>
+
+    <h1 style="font-family: Arial; font-size: 28px; margin-left: 5%; margin-top: 30px;">Temperature trends Boston</h1>
+
+    <p style="font-family: Arial; font-size: 16px; margin-left: 5%;">Created by Yichen Cheng and Zijian Yao
+
+    <button type="button" id="b1" class="button">Refresh</button></p>
+        
+    <div style="color:#ff4c4c; font-size: 64px; text-align: center;" id="chart_year"></div>
+
+    <!--Div that will hold the chart-->
+    <div id="chart_div"></div>
+</html>
+
+
--- index.php
+++ index.php
@@ -23,7 +23,8 @@
         
         function drawChart() {
             var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1cK0UPWvmf23iK3qu3QXrpgcsammpZseROuCN5oHFdO0/edit#gid=0&headers=1');
-            query.setQuery('SELECT A, B, C, D, E, F');
+            // query.setQuery('SELECT A, B, C, D, E, F');
+            query.setQuery('SELECT * LIMIT 366');
             query.send(handleQueryResponse);
         }
  
@@ -37,36 +38,119 @@
             data.addColumn('date', 'Date');
             data.addColumn('number', 'Min');
             data.addColumn('number', 'Delta');
-            data.addColumn('number', '2015 temp');
-            data.addColumn('number', '2015 Min');
-            data.addColumn('number', '2015 Max');
+            
  
             var raw_data = response.getDataTable();
             var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
-    
-            var options = {
-                height: 600,
-                width: 1500,
-                seriesType: 'bars',
-                isStacked: true,
-                series: {
-                    0: { type: 'bars', color: 'white', enableInteractivity: false },
-                    1: { type: 'bars', color: '#aa7243' },
-                    2: { type: 'line', color: 'black' },
-                    3: { type: 'line', color: '#0000e6', lineWidth: 5 },
-                    4: { type: 'line', color: '#e50000', lineWidth: 5 }
-                },
-                hAxis: { 
-                    gridlines: { count: 12, color: 'white' }
-                    //format: 'date' 
-                },
-                vAxis: { gridlines: { count: 6, color: 'white' } },
-                    legend: 'top'
+            var view = new google.visualization.DataView(raw_data);
+            
+            var currentCol = 3;
+            var arr = [0,1,2];
+            var howManyColumns = raw_data.getNumberOfColumns();     // exclude first three columns
+            // console.log("columns in sheet are "+howManyColumns);
+            var year = 1995;
+            var seriesObj = {           // set it as a global object
+                0: { type: 'bars', color: 'white', enableInteractivity: false },    // min value 
+                1: { type: 'bars', color: '#aa7243' }                               // historic range
             };
-    
-            // draw chart
-            chart.draw(raw_data, options);
- 
+            
+            function updateChart() {
+                // year calculator
+                console.log(year);
+                year++;
+                // this is a counter that 
+                var counter = 0;
+                // currentCol % 3 === 0 --> it is a temperature input, corresponding to seriesObj[currentCol - 1]
+                // currentCol % 3 === 1 --> it is a record low input, corresponding to seriesObj[currentCol - 1]
+                // currentCol % 3 === 2 --> it is a record high input, corresponding to seriesObj[currentCol - 1]
+                
+                // preprocessing previous colors - dash it out
+                // use index to count how many indexes should be processed
+                var index;
+                
+                for(index in seriesObj) {
+                    var updateObj = {};
+                   
+                    if (index > 1 && index % 3 === 2) {   // temperature preprocessing
+                        updateObj["color"] = '#e8e8e8';
+                        updateObj["type"] = 'line';
+                        updateObj["lineWidth"] = 0.9;
+                        seriesObj[index] = updateObj;
+                    }
+                    else if (index > 1 && index % 3 === 0) {  // low temp preprocessing
+                        updateObj["color"] = '#c4d3ed';
+                        updateObj["type"] = 'line';
+                        updateObj["lineWidth"] = 3;
+                        seriesObj[index] = updateObj;
+                    }
+                    else if (index > 1 && index % 3 === 1) {  // high temp preprocessing
+                        updateObj["color"] = '#edc4d0';
+                        updateObj["type"] = 'line';
+                        updateObj["lineWidth"] = 3;
+                        seriesObj[index] = updateObj;
+                    }
+                }
+                // this is a step that adds new data into the table
+                while (counter < 3){
+                    // construct inner obj at this step, append it to seriesObj at the end
+                    // append it one at a time
+                    var innerObj = {};
+                    if (currentCol % 3 === 0) {             // daily average temperature column
+                        innerObj["type"] = 'line';
+                        innerObj["color"] = 'black';
+                        data.addColumn("number", "daily");
+                    }
+                    else if (currentCol % 3 === 1) {        // minimum temperature 
+                        innerObj["type"] = 'line';
+                        innerObj["color"] = '#0000e6';
+                        innerObj["lineWidth"] = 5;
+                        data.addColumn("number", "min");
+                    }   
+                    else {                                  // maximum temperature
+                        innerObj["type"] = 'line';
+                        innerObj["color"] = '#e50000';
+                        innerObj["lineWidth"] = 5;
+                        data.addColumn("number", "max");
+                    }
+                    
+                    // variable settings 
+                    seriesObj[currentCol - 1] = innerObj;   // append value to new object
+                    arr.push(currentCol);
+                    view.setColumns(arr);
+                    currentCol++;
+                    counter++;
+                }
+                
+                //console.log("counter value is "+counter);
+                //console.log("array right now is "+arr);
+                
+                var options_update = {
+                    height: 600,
+                    width: 1500,
+                    seriesType: 'bars',
+                    isStacked: true,
+                    series: seriesObj,
+                    hAxis: { 
+                        gridlines: { count: 12, color: 'white' }
+                    },
+                    vAxis: { 
+                        gridlines: { count: 6, color: 'white' } 
+                    },
+                    legend: 'top'
+                };
+                
+                //console.log(options_update);
+                
+                chart.draw(view, options_update);
+               
+                if (currentCol < howManyColumns) {
+                    setTimeout(updateChart, 1000);
+                }
+            }
+            
+            updateChart();
+            
+            
         }
         </script>
     </head>
